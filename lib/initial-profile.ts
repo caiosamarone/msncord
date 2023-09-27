22710d13a767a0b6e05f1ccff9c5eab92f9ace49
Caio Henrique Samarone Pimentel de Melo Mendes <caio.mendes@brf.com>
2023-09-27 11:09:53 -0300

    wip - initial profile

--- lib/initial-profile.ts
+++ lib/initial-profile.ts
@@ -17,5 +17,14 @@ export const initialProfile = async () => {
   if (profile) {
     return profile;
   }
-  //stopped here 1:20:21
+
+  const newProfile = await db.profile.create({
+    data: {
+      userId: user.id,
+      name: `${user.firstName} ${user.lastName}`,
+      imageUrl: user.imageUrl,
+      email: user.emailAddresses[0].emailAddress,
+    },
+  });
+  return newProfile;
 };
